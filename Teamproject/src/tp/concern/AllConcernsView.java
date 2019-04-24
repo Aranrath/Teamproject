0d2e6e7e8342d17f68d6a9b767fc6d8ada64bf85
Aranrath <Karen.A.Wessiepe@gmail.com>
2019-04-24 22:04:24 +0200

    ConcernView...

--- Teamproject/src/tp/concern/AllConcernsView.java
+++ Teamproject/src/tp/concern/AllConcernsView.java
@@ -2,6 +2,8 @@ package tp.concern;
 
 import java.sql.Date;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -9,24 +11,29 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import tp.Presenter;
 import tp.model.Concern;
 
 public class AllConcernsView extends GridPane
 {
 	private Presenter presenter;
+	ObservableList<Concern> allConcerns;
+	ObservableList<Concern> shownConcerns;
 	
 	private TableView<Concern> allConcernsTable;
-	private Label searchBar;
+	private TextField searchTextField;
 	private Button newConcernButton;
 	private Button deleteConcernButton;
+	private CheckBox showClosedConcernsCheckBox;
 	
 	public AllConcernsView(Presenter presenter)
 	{
@@ -40,35 +47,49 @@ public class AllConcernsView extends GridPane
 		setPadding(new Insets(20));
 		setHgap(20);
 		setVgap(20);
+		
+		allConcerns = presenter.getConcerns();
+		shownConcerns = FXCollections.observableArrayList(allConcerns);
+		
 
-		allConcernsTable = new TableView<Concern>(presenter.getConcerns());
-		searchBar = new Label("Hier sollte die SearchBar sein");
+		allConcernsTable = new TableView<Concern>(shownConcerns);
+		searchTextField = new TextField();
+		searchTextField.setPromptText("Suche Anliegen");
 		newConcernButton = new Button("Neues Anliegen hinzufügen");
 		deleteConcernButton = new Button("Anliegen löschen");
-		
+		showClosedConcernsCheckBox = new CheckBox("Zeige abgeschlossene Anliegen");
 		
 		// =====================================================================
 
-		add(searchBar, 0, 0);
-		GridPane.setHalignment(searchBar, HPos.LEFT);
+		add(searchTextField, 0, 0);
+		GridPane.setHalignment(searchTextField, HPos.LEFT);
+		
+		add(newConcernButton, 1, 0);
+		GridPane.setHalignment(newConcernButton, HPos.RIGHT);
 
 		add(allConcernsTable, 0, 1, 2, 1);
 		
-		add(newConcernButton, 1, 2);
-		GridPane.setHalignment(newConcernButton, HPos.RIGHT);
+		add(showClosedConcernsCheckBox, 1, 2);
+		GridPane.setHalignment(showClosedConcernsCheckBox, HPos.RIGHT);
 
 		add(deleteConcernButton, 0, 2);
 		GridPane.setHalignment(deleteConcernButton, HPos.LEFT);
 
 
 		//===================================================================
-
-		ColumnConstraints col0 = new ColumnConstraints();
-		col0.setPercentWidth(30);
-		ColumnConstraints col1 = new ColumnConstraints();
-		col1.setPercentWidth(30);
+		//constraints
+		
+		ColumnConstraints col = new ColumnConstraints();
+		col.setPercentWidth(100 / 2);
 		     
-		getColumnConstraints().addAll(col0,col1);
+		getColumnConstraints().addAll(col,col);
+		
+		RowConstraints buttonRow = new RowConstraints();
+		buttonRow.setPercentHeight(20 / 2);
+		RowConstraints tableRow = new RowConstraints();
+		tableRow.setPercentHeight(80);
+		     
+		getRowConstraints().addAll(buttonRow,tableRow,buttonRow);
 		
 		// ======================================================================
 		
@@ -114,6 +135,15 @@ public class AllConcernsView extends GridPane
 
 		});
 		
+		showClosedConcernsCheckBox.setOnAction(event -> {
+			filterConcerns(searchTextField.getText());
+		});
+		
+		searchTextField.textProperty().addListener((obs, oldText, newText) -> {
+			filterConcerns(newText);
+		});
+		
+		
 		allConcernsTable.setOnMousePressed(new EventHandler<MouseEvent>() {
 		    @Override 
 		    public void handle(MouseEvent event) {
@@ -133,9 +163,71 @@ public class AllConcernsView extends GridPane
 	
 
 
+	private void filterConcerns(String searchTerm) {
+		
+		shownConcerns.clear();
+		
+		if(searchTerm.isEmpty())
+		{
+			
+			if(showClosedConcernsCheckBox.isSelected())
+			{
+				shownConcerns.addAll(allConcerns);
+			}
+			else
+			{
+				for (Concern concern : allConcerns)
+				{
+					if(!concern.isClosed())
+					{
+						shownConcerns.add(concern);
+					}
+					
+				}
+			}
+	
+		}
+		else
+		{
+			
+			String [] searchTerms = searchTerm.toLowerCase().split(" ");
+			
+			if(showClosedConcernsCheckBox.isSelected())
+			{
+				for (Concern concern : allConcerns)
+				{
+					if(ConcernView.containsAll(concern.toString().toLowerCase(), searchTerms))
+					{
+						shownConcerns.add(concern);
+					}
+					
+				}
+			}
+			else
+			{
+				for (Concern concern : allConcerns)
+				{
+					if(!concern.isClosed() && ConcernView.containsAll(concern.toString().toLowerCase(), searchTerms))
+					{
+						shownConcerns.add(concern);
+					}
+					
+				}
+			}
+			
+			
+			
+		}
+		
+		
+		
+	}
+
 	public void updateView() {
-		allConcernsTable.setItems(presenter.getConcerns());
-		//TODO updateSearchbar
+		allConcerns = presenter.getConcerns();
+		shownConcerns = FXCollections.observableArrayList(allConcerns);
+		filterConcerns(searchTextField.getText());
+		
 	}
 
 	
